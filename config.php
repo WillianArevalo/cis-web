f59d540bca3d6e86d032934034cb3f89ac52f9d6
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-04-05 14:15:03 -0600

    Se agrego información de la bd

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 define("URL_SITIO", "https://cis-tacachico.zeabur.app");
-define("MYSQL_HOST", "localhost");
+define("MYSQL_HOST", "roundhouse.proxy.rlwy.net");
 define("MYSQL_USER", "root");
-define("MYSQL_PASSWORD", "");
-define("MYSQL_DATABASE", "cis");
+define("MYSQL_PASSWORD", "wfyXGFGdKUtJXRnsVgmUGNXNqIhywOPu");
+define("MYSQL_DATABASE", "CIS");
